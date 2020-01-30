33c9b34172fc87c1c8f85c59d1733c808f39d6c3
Ben Johnson-Laird <benjohnsonlaird@gmail.com>
2020-01-30 14:23:09 -0500

    Added questionnaire data, parameterized a lot of functions with dataframe

--- decisionTree.py
+++ decisionTree.py
@@ -1,7 +1,9 @@
 import re
+import urllib
 
 import pandas as pd
 import numpy as np
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 from sklearn.tree import export_graphviz
 
 import pydot
@@ -13,8 +15,14 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.impute import SimpleImputer
 
 from imblearn.combine import SMOTETomek
+import matplotlib.pyplot as plt
 
-nhanes_2013_df = pd.DataFrame()
+from yellowbrick.datasets import load_credit
+from yellowbrick.classifier import classification_report as yb_class_report
+
+# nhanes_2013_df = pd.DataFrame()
+from tabulate import tabulate
+
 
 pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 500)
@@ -35,14 +43,36 @@ hanes_2013_files = ['ALB_CR_H.txt', 'ALDS_H.txt', 'ALD_H.txt', 'AMDGDS_H.txt', '
                     'UTASS_H.txt', 'UTAS_H.txt', 'UVOCS_H.txt', 'UVOC_H.txt', 'VID_H.txt', 'VITB12_H.txt',
                     'VNAS_H.txt', 'VNA_H.txt', 'VOCWBS_H.txt', 'VOCWB_H.txt', ]
 
+"""
+hanes_2013_questionnaire_files = ['ACQ_H.txt', 'ALQ_H.txt', 'BPQ_H.txt', 'CBQ_H.txt', 'CDQ_H.txt', 'CFQ_H.txt',
+                                  'CKQ_H.txt', 'CSQ_H.txt', 'DBQ_H.txt', 'DEQ_H.txt', 'DIQ_H.txt', 'DLQ_H.txt',
+                                  'DPQ_H.txt', 'DUQ_H.txt', 'ECQ_H.txt', 'FSQ_H.txt', 'HEQ_H.txt', 'HIQ_H.txt',
+                                  'HOQ_H.txt', 'HSQ_H.txt', 'HUQ_H.txt', 'IMQ_H.txt', 'INQ_H.txt', 'KIQ_U_H.txt',
+                                  'MCQ_H.txt', 'OCQ_H.txt', 'OHQ_H.txt', 'OSQ_H.txt', 'PAQ_H.txt', 'PFQ_H.txt',
+                                  'PUQMEC_H.txt', 'RHQ_H.txt', 'RXQASA_H.txt', 'RXQ_RX_H.txt',
+                                  'SLQ_H.txt',
+                                  'SMQFAM_H.txt', 'SMQRTU_H.txt', 'SMQSHS_H.txt', 'SMQ_H.txt', 'SXQ_H.txt', 'VTQ_H.txt',
+                                  'WHQMEC_H.txt', 'WHQ_H.txt',
+                                  ]
+"""
+
+hanes_2013_questionnaire_files = ['ACQ_H.txt', 'ALQ_H.txt', 'BPQ_H.txt', 'CBQ_H.txt', 'CDQ_H.txt', 'CFQ_H.txt',
+                                  'CKQ_H.txt', 'CSQ_H.txt', 'DBQ_H.txt', 'DEQ_H.txt', 'DIQ_H.txt', 'DLQ_H.txt',
+                                  'DPQ_H.txt', 'DUQ_H.txt', 'ECQ_H.txt', 'FSQ_H.txt', 'HEQ_H.txt', 'HIQ_H.txt',
+                                  'HOQ_H.txt', 'HSQ_H.txt', 'HUQ_H.txt', 'IMQ_H.txt', 'INQ_H.txt', 'KIQ_U_H.txt',
+                                  'MCQ_H.txt', 'OCQ_H.txt', 'OHQ_H.txt', 'OSQ_H.txt', 'PAQ_H.txt', 'PFQ_H.txt',
+                                  'PUQMEC_H.txt', 'RHQ_H.txt', 'RXQASA_H.txt', 'RXQ_RX_H.txt',
+                                  'SLQ_H.txt',
+                                  'SMQFAM_H.txt', 'SMQRTU_H.txt', 'SMQSHS_H.txt', 'SMQ_H.txt', 'SXQ_H.txt', 'VTQ_H.txt',
+                                  'WHQMEC_H.txt', 'WHQ_H.txt',
+                                  ]
+
 codebooks_dict = {}
-with open('codebooks.json', 'r') as f:
+with open('combined_codebook.json', 'r') as f:
     codebooks_dict = json.load(f)
 
 
 def read_mortality_data():
-    global nhanes_2013_df
-
     # from pandas.api.extensions import ExtensionDtype
     dir = "data/"
     file = "NHANES_2013_2014_MORT_2015_PUBLIC.dat"
@@ -100,23 +130,24 @@ def read_mortality_data():
     dtype = {'seqn': int, 'eligstat': int, 'mortstat': float, 'ucod_leading': float,
              'diabetes': float, 'hyperten': float, 'dodqtr': float, 'dodyear': float,
              'wgt_new': float, 'sa_wgt_new': float, 'permth_int': float, 'permth_exm': float}
-    nhanes_2013_df = pd.read_fwf(dir + file, names=cols, colspecs=cspecs, dtype=dtype,
-                                 na_values=["."])
+    df = pd.read_fwf(dir + file, names=cols, colspecs=cspecs, dtype=dtype,
+                     na_values=["."])
+    return df
 
 
-def read_and_merge_data(file):
-    global nhanes_2013_df
+def read_and_merge_data(dir_prefix, file, df):
+    debug = False
     file_prefix_pat = re.compile('([\w]+)')
 
-    hanes_2013_dir = 'nhanes-2013/'
-    new_df = pd.read_sas(hanes_2013_dir + file, format='xport')
+    new_df = pd.read_sas(dir_prefix + file, format='xport')
     # Map the lowering function to all column names
     new_df.columns = map(str.lower, new_df.columns)
 
     for c in new_df.columns:
         # If any column has "sample" in the description remove it
         if c in codebooks_dict and ("sample" in codebooks_dict[c].lower() or "weights" in codebooks_dict[c].lower()):
-            print(f"dropping: {c}")
+            if debug:
+                print(f"dropping: {c}")
             new_df.drop(labels=c, axis=1, inplace=True)
 
     # Prefix all the columns except seqn with the file prefix
@@ -124,75 +155,74 @@ def read_and_merge_data(file):
     prefix = key_text.group(1).lower()
     new_df.columns = map(lambda x: x if x == 'seqn' else x + "_" + prefix, new_df.columns)
 
-    print(f"new columns: {new_df.columns}")
+    if debug:
+        print(f"new columns: {new_df.columns}")
     # Convert sequence number to int
     new_df = new_df.astype({'seqn': 'int'})
-    nhanes_2013_df = pd.merge(nhanes_2013_df, new_df, how='left',
-                              left_on=['seqn'],
-                              right_on=['seqn'])
-    return
+    df = pd.merge(df, new_df, how='left',
+                  left_on=['seqn'],
+                  right_on=['seqn'])
+    return df
 
 
 # nhanes_2013_df = read_and_merge_data(nhanes_2013_df, 'DEMO_H.txt')
 # nhanes_2013_df = read_and_merge_data(nhanes_2013_df, 'HDL_H.txt')
 
-def merge_all_data():
+def merge_all_data(dir_prefix, files, df):
     debug = False
     count = 0
     start = 60
     end = 90
     if not debug:
         start = 0
-        end = len(hanes_2013_files)
+        end = len(files)
     # Skip files that don't have seqn as a key
-    skip_files = ['BFRPOL_H.txt', 'PCBPOL_H.txt', 'PSTPOL_H.txt']
-    for i in range(start, min(len(hanes_2013_files), end)):
-        f = hanes_2013_files[i]
-        # for f in hanes_2013_files:
+    skip_files = ['BFRPOL_H.txt', 'PCBPOL_H.txt', 'PSTPOL_H.txt', 'RXQ_DRUG.txt']
+    for i in range(start, min(len(files), end)):
+        f = files[i]
+        # for f in files:
         if f in skip_files:
             continue
-        print(f"Merging: {f}")
-        read_and_merge_data(f)
+
+        # print(f"Merging: {f}")
+        df = read_and_merge_data(dir_prefix, f, df)
         count += 1
+    return df
 
 
-def impute_mean():
+def impute_mean(df):
     """
     Completely nan columns must have been dropped already
     :return:
     """
-    global nhanes_2013_df
 
-    nhanes_2013_df.dropna(axis=1, how='all', inplace=True)
+    df.dropna(axis=1, how='all')
     imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-    imp_mean = imp_mean.fit(nhanes_2013_df)
+    imp_mean = imp_mean.fit(df)
 
-    new_df = pd.DataFrame(imp_mean.transform(nhanes_2013_df))
-    new_df.columns = nhanes_2013_df.columns
-    new_df.index = nhanes_2013_df.index
-    nhanes_2013_df = new_df
+    new_df = pd.DataFrame(imp_mean.transform(df))
+    new_df.columns = df.columns
+    new_df.index = df.index
+    return new_df
 
 
-def drop_interpolate():
-    global nhanes_2013_df
+def drop_interpolate(df):
     # Drop all values where eligstat != 1
     # nhanes_2013_df = nhanes_2013_df.drop(nhanes_2013_df[nhanes_2013_df['eligstat'] != 1].index)
-    nhanes_2013_df.drop(nhanes_2013_df[nhanes_2013_df['eligstat'] != 1].index, inplace=True)
+    df.drop(df[df['eligstat'] != 1].index, inplace=True)
 
     # Drop all NA columns first
     # nhanes_2013_df = nhanes_2013_df.dropna(axis=1, how='all')
-    nhanes_2013_df.dropna(axis=1, how='all', inplace=True)
-    impute_mean()
+    df.dropna(axis=1, how='all', inplace=True)
 
-    ## BAD!!!
-    # means = nhanes_2013_df.mean()
-    # nhanes_2013_df = nhanes_2013_df.fillna(means, inplace=True)
-    # nhanes_2013_df.fillna(0, inplace=True)
+    #TODO - look at whether dropping object-type columns can be avoided
+    df = df.select_dtypes(exclude=['object'])
 
-    # nhanes_2013_df = nhanes_2013_df.dropna()
+    df = impute_mean(df)
 
     # Set index column
-    nhanes_2013_df.set_index('seqn')
+    df.set_index('seqn')
+    return df
 
 
 def smote_classify(X, y):
@@ -215,23 +245,22 @@ def smote_classify(X, y):
     return X_res, y_res
 
 
-def train_model():
-    smote = False
+def train_model(df, smote=False):
     # Drop predictor dependent columns from training set
-    train_cols_to_drop = ['mortstat', 'ucod_leading', 'permth_int', 'permth_exm', 'diabetes',
+    train_cols_to_drop = ['seqn', 'mortstat', 'ucod_leading', 'permth_int', 'permth_exm', 'diabetes',
                           'hyperten']
-    X = nhanes_2013_df.drop(labels=train_cols_to_drop, axis=1)
-    y = nhanes_2013_df['mortstat']
+    X = df.drop(labels=train_cols_to_drop, axis=1)
+    y = df['mortstat']
 
-    # Split first
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=123)
+    # Split first - random state was 123
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=456)
 
     if smote:
         X_smote, y_smote = smote_classify(X_train, y_train)
     else:
         X_smote, y_smote = (X_train, y_train)
 
-    tree_clf = DecisionTreeClassifier(criterion='gini', max_depth=5)
+    tree_clf = DecisionTreeClassifier(criterion='gini', max_depth=7)
     tree_clf.fit(X_smote, y_smote)
 
     estimator = tree_clf
@@ -252,7 +281,7 @@ def get_feature_names(X_train):
             if prefix in codebooks_dict:
                 feat_names.append(f"{codebooks_dict[prefix][0:20]} ({c})")
             else:
-                print(f"Missing key: {c}")
+                # print(f"Missing key: {c}")
                 feat_names.append(f"({c})")
     else:
         feat_names = X_train.columns
@@ -261,7 +290,6 @@ def get_feature_names(X_train):
 
 
 def create_png(filename, X_train, estimator):
-
     feat_names = get_feature_names(X_train)
 
     # Export as dot file
@@ -286,7 +314,6 @@ from collections import Counter
 
 def print_test_tree(in_file, out_file, X_test, y_test, clf):
     feat_names = get_feature_names(X_test)
-
 
     (graph,) = pydot.graph_from_dot_file(in_file)
 
@@ -317,22 +344,71 @@ def print_test_tree(in_file, out_file, X_test, y_test, clf):
                     labels[i] = 'samples = {}'.format(int(label.split('=')[1]) + 1)
 
             node.set('label', '\\n'.join(labels))
-        print(f"leaf node: {labels}")
+        # print(f"leaf node: {labels}")
     graph.write_png(out_file)
 
 
-def main():
-    read_mortality_data()
-    merge_all_data()
+def plot_feature_importances(model, X_train):
+    n_features = X_train.shape[1]
+    plt.figure(figsize=(8,8))
+    plt.barh(range(n_features), model.feature_importances_, align='center')
+    plt.yticks(np.arange(n_features), X_train.columns.values)
+    plt.xlabel('Feature importance')
+    plt.ylabel('Feature')
 
-    drop_interpolate()
+
+def bagging_classifier(X_train, y_train, X_test, y_test):
+    bagged_tree = BaggingClassifier(DecisionTreeClassifier(criterion='gini', max_depth=5),
+                                    n_estimators=20)
+
+    # Fit to the training data
+    bagged_tree.fit(X_train, y_train)
+
+    # Training & testing accuracy score
+    train_accuracy = bagged_tree.score(X_train, y_train)
+    test_accuracy = bagged_tree.score(X_test, y_test)
+    print(f"Bagging classifier - train accuracy: {train_accuracy}  test_accuracy: {test_accuracy}")
+
+
+def random_forest_classifier(X_train, y_train, X_test, y_test):
+    forest = RandomForestClassifier(n_estimators=100, max_depth=5)
+    forest.fit(X_train, y_train)
+
+    # Training & testing accuracy score
+    train_accuracy = forest.score(X_train, y_train)
+    test_accuracy = forest.score(X_test, y_test)
+    print(f"Random forest classifier - train accuracy: {train_accuracy}  test_accuracy: {test_accuracy}")
+    return forest
+
+
+def yb_classification_report(note, tree_clf, X_test, y_test):
+    print(note)
+    visualizer = yb_class_report(tree_clf, X_test, y_test)
+    visualizer.show()
+
+
+def main():
+    # global nhanes_2013_df
+    hanes_2013_dir = 'nhanes-2013/'
+    hanes_2013_questionnaire_dir = 'nhanes-2013-questionnaire/'
+
+    mort_df = read_mortality_data()
+    # nhanes_2013_df = merge_all_data(hanes_2013_dir, hanes_2013_files, mort_df)
+    nhanes_2013_df = merge_all_data(hanes_2013_questionnaire_dir,  hanes_2013_questionnaire_files, mort_df)
+
+    # print(tabulate(nhanes_2013_df, headers='keys', tablefmt='psql'))
+    # print(nhanes_2013_df.dtypes)
+
+    print(f"After merge: {nhanes_2013_df.shape}")
+
+    nhanes_2013_df = drop_interpolate(nhanes_2013_df)
+
+    print(f"After drop: {nhanes_2013_df.shape}")
+
+    tree_clf, X_train, X_test, y_train, y_test = train_model(nhanes_2013_df, smote=True)
 
     print(f"Len of full data frame: {len(nhanes_2013_df)}")
-
-    tree_clf, X_train, X_test, y_train, y_test = train_model()
     print(f"Len of X_train: {len(X_train)} len of X_test: {len(X_test)}")
-
-    input("Continue?")
 
     # Train set predictions
     print("*** Training: ***")
@@ -351,6 +427,12 @@ def main():
     print(classification_report(y_test, test_pred))
 
     print_test_tree("train_tree.dot", "test_tree.png", X_test, y_test, tree_clf)
+    print(f"Len of X_train: {len(X_train)} len of X_test: {len(X_test)}")
+
+    bagging_classifier(X_train, y_train, X_test, y_test)
+    forest = random_forest_classifier(X_train, y_train, X_test, y_test)
+
+    yb_classification_report("Forest classification", forest, X_test, y_test)
+
 
 main()
-
